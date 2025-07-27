f61ddff907e5b3293dbb5f4c5d53a38c2821a32f
danifc123 <danielcarmo29@gmail.com>
2025-07-27 00:06:58 -0300

    Feat: preparando ambiente com angular material e criando o component input

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LayoutAuthComponent } from './layout/layout-auth/layout-auth.component';
+export const routes: Routes = [
+  {
+    path: '',
+    component:LayoutAuthComponent,
+    children: []
+  },
+];
