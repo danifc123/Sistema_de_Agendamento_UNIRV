894689e3ab53e1a30dfcd33aa266e5f317e76def
danifc123 <danielcarmo29@gmail.com>
2025-07-27 11:39:13 -0300

    style: ajustando layaout do register

--- Frontend/src/app/models/register/register.component.ts
+++ Frontend/src/app/models/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { InputComponent } from "../../components/input/input.component";
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
