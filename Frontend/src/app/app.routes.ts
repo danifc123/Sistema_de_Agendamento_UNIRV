9d6368b3c6e38354ab2d94c9dba97622a529bdc2
danifc123 <danielcarmo29@gmail.com>
2025-07-27 01:39:26 -0300

    feat: fazendo a ligaçao entre as rotas pelo angular

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { LayoutAuthComponent } from './layout/layout-auth/layout-auth.component';
+import { LoginComponent } from './models/login/login.component';
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
+    title: 'Agendamento UNIRV - Login',
     component:LayoutAuthComponent,
-    children: []
+    children: [
+      {
+        path:'',
+        component:LoginComponent
+      }
+    ]
   },
 ];
