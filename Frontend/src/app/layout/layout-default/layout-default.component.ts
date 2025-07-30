0ddd946c2ff9af41d937f6813cc76c5a5fdcf2ca
danifc123 <danielcarmo29@gmail.com>
2025-07-30 14:44:32 -0300

    feat: atribuindo caracteristicas ao componente de tabela

--- Frontend/src/app/layout/layout-default/layout-default.component.ts
+++ Frontend/src/app/layout/layout-default/layout-default.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
-import { CalendarioComponent } from "../../components/calendario/calendario.component";
+ import { TabelaAgendamentoComponent } from "../../components/tabela-agendamento/tabela-agendamento.component";
 
 
 @Component({
   selector: 'app-layout-default',
-  imports: [MatIconModule, CalendarioComponent],
+  imports: [MatIconModule, TabelaAgendamentoComponent],
   templateUrl: './layout-default.component.html',
   styleUrl: './layout-default.component.scss'
 })
