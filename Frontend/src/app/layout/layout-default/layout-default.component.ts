378e07490f58d305dd8d5610ba1f6078ab244f24
danifc123 <danielcarmo29@gmail.com>
2025-07-30 08:53:21 -0300

    feat/style: adicionando calendario ao default layout

--- Frontend/src/app/layout/layout-default/layout-default.component.ts
+++ Frontend/src/app/layout/layout-default/layout-default.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { CalendarioComponent } from "../../components/calendario/calendario.component";
 
 
 @Component({
   selector: 'app-layout-default',
-  imports: [MatIconModule,],
+  imports: [MatIconModule, CalendarioComponent],
   templateUrl: './layout-default.component.html',
   styleUrl: './layout-default.component.scss'
 })
