a8dc751a4076e595f81d5fe437df8cd5a59fdc87
danifc123 <danielcarmo29@gmail.com>
2025-07-30 19:31:36 -0300

    feat: adicionando componente de agenda

--- Frontend/src/app/layout/layout-default/layout-default.component.ts
+++ Frontend/src/app/layout/layout-default/layout-default.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
- import { TabelaAgendamentoComponent } from "../../components/tabela-agendamento/tabela-agendamento.component";
-import { CalendarioComponent } from "../../components/calendario/calendario.component";
 
 
 @Component({
   selector: 'app-layout-default',
-  imports: [MatIconModule, TabelaAgendamentoComponent, CalendarioComponent],
+  imports: [MatIconModule],
   templateUrl: './layout-default.component.html',
   styleUrl: './layout-default.component.scss'
 })
