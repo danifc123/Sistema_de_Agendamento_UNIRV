617093885c2bd4c7c3c5804833549ac573e9f428
danifc123 <danielcarmo29@gmail.com>
2025-07-30 08:34:56 -0300

    style: criando e ajustando layout default

--- Frontend/src/app/layout/layout-default/layout-default.component.ts
+++ Frontend/src/app/layout/layout-default/layout-default.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterOutlet } from "../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
+
 
 @Component({
   selector: 'app-layout-default',
-  imports: [],
+  imports: [MatIconModule, ],
   templateUrl: './layout-default.component.html',
   styleUrl: './layout-default.component.scss'
 })
