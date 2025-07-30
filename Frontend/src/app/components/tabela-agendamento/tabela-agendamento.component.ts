13f06d2d061a47425b07229b82711d0aea75e56e
danifc123 <danielcarmo29@gmail.com>
2025-07-30 10:50:53 -0300

    feat: criando novo componente de tabela dos alunos com agendamentos

---
+++ Frontend/src/app/components/tabela-agendamento/tabela-agendamento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-agendamento',
+  imports: [],
+  templateUrl: './tabela-agendamento.component.html',
+  styleUrl: './tabela-agendamento.component.scss'
+})
+export class TabelaAgendamentoComponent {
+
+}
