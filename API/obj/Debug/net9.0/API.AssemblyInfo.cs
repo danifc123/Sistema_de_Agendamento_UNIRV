23a8523cdb7586f9f76300c6e545d1bb3667bfc0
Daniel Faria <92318406+danifc123@users.noreply.github.com>
2025-08-11 18:51:42 -0300

    Finalizando dia

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898900181c50d06b4b5c24c00a326c4a0ed63368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87929c6d2f39524cbf75f4de1374bb977fd0292e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
