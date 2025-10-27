331729472c40f0ea9fa7514ad320be726a716e9a
danifc123 <danielcarmo29@gmail.com>
2025-10-27 20:52:00 -0300

    database deploy

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6967ac34784849a7eb9be485061d128145f420ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f17298535e2cccbd33dea60f3cb248bda14e51")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
