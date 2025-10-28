385b37dae216b3d4af60e866ebd4fef00e6fed8e
danifc123 <danielcarmo29@gmail.com>
2025-10-28 10:03:45 -0300

    console log

--- API/Program.cs
+++ API/Program.cs
@@ -50,7 +50,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 // Configurar JWT Authentication
 var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
 var key = Encoding.ASCII.GetBytes(jwtSettings?.SecretKey ?? "3a964dea4e97f5c3c1c780973874bf63fdb193857adb6356e779a51b19f804fb");
-
+Console.WriteLine("Key: " + key);
 builder.Services.AddAuthentication(x =>
 {
     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
