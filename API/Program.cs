12f5bbe66a22b63ae0ece6d598f8f999b3c4ddec
danifc123 <danielcarmo29@gmail.com>
2025-10-28 09:44:38 -0300

    jwt statica

--- API/Program.cs
+++ API/Program.cs
@@ -118,15 +118,3 @@ using (var scope = app.Services.CreateScope())
 app.MapControllers();
 
 app.Run();
-
-// Método de conversão hexadecimal para byte[]
-static byte[] ConvertHexToBytes(string hex)
-{
-    int length = hex.Length;
-    byte[] bytes = new byte[length / 2];
-    for (int i = 0; i < length; i += 2)
-    {
-        bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
-    }
-    return bytes;
-}
