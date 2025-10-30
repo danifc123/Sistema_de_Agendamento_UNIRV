c80014592d939e5d67666b2c9fb5804249b8a193
danifc123 <danielcarmo29@gmail.com>
2025-10-30 09:30:46 -0300

    revertendo login de ngmodel para value

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb3541ae8a320dd5ef175d5cb896ecd36a09dc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45077c3296e96a19767b83895cec575fc50d828f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
