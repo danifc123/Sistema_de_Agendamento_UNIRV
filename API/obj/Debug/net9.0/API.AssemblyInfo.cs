149d0951800ac15d54174c98148eecd90df1c5ec
danifc123 <danielcarmo29@gmail.com>
2025-10-31 08:46:25 -0300

    ajustando botao de login

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3f87c490c74aadf4d755ab49f36267eedb0f39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80c434fa04cf500c15493a07c3182f1c74db237")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
